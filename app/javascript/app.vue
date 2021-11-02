fb87ac30fd9e8f31cce9d54e772f875c0266b40f
akachiryo <rirugameri@icloud.com>
2021-11-02 14:51:05 +0900

    微調整

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -42,33 +42,6 @@
     </v-app-bar>
 
     <v-main>
-      <!-- <v-container
-              class="fill-height"
-              fluid
-      >
-        <v-row
-                align="center"
-                justify="center"
-        >
-          <v-col class="text-center"> -->
-            <!-- <v-tooltip left>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                        :href="source"
-                        icon
-                        large
-                        target="_blank"
-                        v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip> -->
-            <!-- <router-view></router-view>
-          </v-col>
-        </v-row>
-      </v-container> -->
       <router-view></router-view>
     </v-main>
     <v-footer
